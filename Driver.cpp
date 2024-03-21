d992422616d73c0d04ae7c124dff5ecf4dde14e4
Peter <frdmanp@gmail.com>
2024-03-21 17:29:22 -0400

    idk, progress probably.

--- Driver.cpp
+++ Driver.cpp
@@ -4,14 +4,29 @@
 */
 
 #include "algorithms/Wilsons.cpp"
+#include "algorithms/HK.cpp"
+#include <string>
 
 
 int 
 main(int argc, char* argv[])
 {
     int dimension = std::stoi(argv[1]);
-    Wilsons wilson(dimension, dimension);
-    wilson.run();
-    std::cout << wilson;
+    std::string w("w");
+    if (argc == 3 && w.compare(argv[2]))
+    {
+        std::cout << "Wilsons Algorithm Maze: \n";
+        Wilsons wilson(dimension, dimension);
+        wilson.run();
+        std::cout << wilson;
+    } 
+    else 
+    {
+        std::cout << "Hunt and Kill Algorithm Maze: \n";
+        HK hk(dimension, dimension);
+        hk.run();
+        std::cout << hk; 
+    }
+    
 }
 
--- algorithms/HK.cpp
+++ algorithms/HK.cpp
@@ -6,115 +6,146 @@
 
 class HK
 {
+    //TODO: Replace this with a call that gets time after testing is done
     const static int RNG_SEED = 0;
     Maze mz;
-    unsigned lastIdx;
-    std::minstd_rand rand;
+    int lastIdx;
+    std::minstd_rand r;
 
     public: 
 
         //ctor
         HK (Maze &mz)
-        :mz(mz), lastIdx(0), rand(RNG_SEED) {}
+        :mz(mz), lastIdx(0), r(RNG_SEED) {}
+
+        HK (int rowCount, int colCount)
+        :mz(rowCount, colCount), lastIdx(0), r(RNG_SEED) {}
 
         //Runs the algorithm on the provided maze
         void
         run()
         {
+            mz.openStart();
+            int startIdx = r() % mz.size();
             while(lastIdx != mz.size() - 1)
             {
-                randomWalk(lastIdx);
-                hunt();
+                randomWalk(startIdx);
+                startIdx = hunt();
             }
+            mz.openEnd();
         }
         
         friend std::ostream& operator<<(std::ostream& os, const HK &hk);
 
     private:
-         enum
-         DIRECTION { UP, DOWN, LEFT, RIGHT };
 
-    /** Performs a random walk on maze mz. 
-
-    NOTE: this method assumes that at least one cell in the maze has 
-    been modified, making that the end of the random walk
-
-    @p mz - the maze to be walked
-    @p eraseLoops - a bool deciding whether or not loops are erased
-
-    @return a list of indices representing the walk path
-    */
+    /** Performs a random walk on maze mz, starting at startIdx. */
     void
-    randomWalk (unsigned startIdx)
+    randomWalk (int startIdx)
     {
-        std::minstd_rand rand(RNG_SEED);
+        int prev;
+        int cur = startIdx;
 
-        unsigned curIdx = startIdx;
-        std::set<unsigned> invalidDirs;
-        int nextIdx;
-        unsigned candidate;
-
-        while (invalidDirs.size() < 4)
+        while (!mz.hasCell(cur))
         {
-            candidate = rand() % 4;
+            prev = cur;
+            cur = validStep(cur, prev);
+            mz.connect(cur, prev);
+        }
 
-            //if the candidate direction has been checked, don't check again
-            if (invalidDirs.contains(candidate)) { continue; }
+    }
 
-            switch(candidate)
+    int
+    validStep(int cur, int prev)
+    {
+        if(!mz.hasIndex(cur) || !mz.hasIndex(prev)) { return mz.size(); }
+
+        std::set <DIRECTION> visitedDirs;
+        int next = mz.size();
+        int nextIdx;
+        while (!mz.hasIndex(next) && visitedDirs.size() < 4)
+        {
+            int dir = r() % 4;
+            switch (dir)
             {
-                case UP: 
-                    invalidDirs.insert(UP);
-                    nextIdx = curIdx - mz.width();
-                    if(nextIdx >= 0 && mz[nextIdx].val() == 0)
+                case UP:
+                
+                    visitedDirs.emplace(UP);
+                    nextIdx = mz.getIdx(cur, UP);
+                    if(mz.validMove(cur, UP) && nextIdx != prev)
                     {
-                        mz.connect(curIdx, nextIdx);
-                        curIdx = nextIdx;
+                        return nextIdx;
                     }
-                    break; 
+                    break;
                 case DOWN: 
-                    invalidDirs.insert(DOWN);
-                    nextIdx = curIdx + mz.width();
-                    if(nextIdx < mz.size() && mz[nextIdx].val() == 0)
+                    visitedDirs.emplace(DOWN);
+                    nextIdx = mz.getIdx(cur, DOWN);
+                    if(mz.validMove(cur, DOWN) && nextIdx != prev)
                     {
-                        mz.connect(curIdx, nextIdx);
-                        curIdx = nextIdx;
+                        return nextIdx;
                     }
                     break;
                 case LEFT:
-                    invalidDirs.insert(LEFT);
-                    nextIdx = curIdx % mz.width() - 1;
-                    if (nextIdx >= 0 && mz[nextIdx].val() == 0)
+                    visitedDirs.emplace(LEFT);
+                    nextIdx = mz.getIdx(cur, LEFT);
+                    if(mz.validMove(cur, LEFT) && nextIdx != prev)
                     {
-                       mz.connect(curIdx, nextIdx);
-                       curIdx = nextIdx;
+                        return nextIdx;
                     }
                     break;
                 case RIGHT: 
-                    invalidDirs.insert(RIGHT);
-                    nextIdx = curIdx % mz.width() + 1;
-                    if (nextIdx < mz.width() && mz[nextIdx].val() == 0)
+                    visitedDirs.emplace(RIGHT);
+                    nextIdx = mz.getIdx(cur, RIGHT);
+                    if(mz.validMove(cur, RIGHT) && nextIdx != prev)
                     {
-                       mz.connect(curIdx, nextIdx);
-                       curIdx = nextIdx;
+                        return nextIdx;
                     }
                     break;
             }
-
-            //if they are equal, a new cell was connected to the maze
-            if(curIdx == nextIdx) { invalidDirs.clear(); }
         }
+        return next;
     }
 
     /** Hunts for the next cell not currently in the maze that is next to a connected cell. 
         Updates lastIdx as it goes */
-    void
+    int
     hunt()
     {
+        lastIdx = 0;
         while (mz[lastIdx].val() != 0)
         {
+            int neighbor = connectedNeighbor(lastIdx);
+            if (mz.hasCell(neighbor))
+            {
+                mz.connect(lastIdx, neighbor);
+                return lastIdx;
+            }
             ++lastIdx;
         }
+        return mz.size();
+    }
+
+    /** Helper to find a neighbor that has been seen already. */
+    int
+    connectedNeighbor(int cur)
+    {
+        if (mz.validMove(cur, UP) && mz.hasCell(mz.getIdx(cur, UP)))
+        {
+            return mz.getIdx(cur, UP);
+        }
+        if (mz.validMove(cur, DOWN) && mz.hasCell(mz.getIdx(cur, DOWN)))
+        {
+            return mz.getIdx(cur, DOWN);
+        }
+        if (mz.validMove(cur, LEFT) && mz.hasCell(mz.getIdx(cur, LEFT)))
+        {
+            return mz.getIdx(cur, LEFT);
+        }
+        if (mz.validMove(cur, RIGHT) && mz.hasCell(mz.getIdx(cur, RIGHT)))
+        {
+            return mz.getIdx(cur, RIGHT);
+        }
+        return mz.size();
     }
 };
 
