af8f1ae3214d8b98fd0ef2288ac01d7eaadc79f4
Peter <frdmanp@gmail.com>
2024-07-03 15:55:47 -0400

    notes for improvements to API

--- verification.py
+++ verification.py
@@ -1,5 +1,9 @@
 from include.PyExternBindings import genEmptyMaze, printMaze, genWilsons
 
+#TODO: move seed generation into python, then return the maze after generation. 
+#This will allow chaining like print(gen(emptyMaze(), seed))
+
+#TODO: give the python maze an __str__ so that you can print it with the built print
 maze = genEmptyMaze(5,5)
 genWilsons(maze, 0)
 printMaze(maze)
