291b9dc431da18321aa801c730b16008fb73dd87
Peter <frdmanp@gmail.com>
2024-08-29 11:15:12 -0400

    ugh Ok now I have the output of CMake but I need to get it into Python I think?

--- src/lib/bindings/Maze_Bindings.cpp
+++ src/lib/bindings/Maze_Bindings.cpp
@@ -9,7 +9,7 @@ PYBIND11_MODULE(Cell, C)
     C.doc() = "A pybind module wrapping a 4-bit cell implementation";
     py::class_<Cell>(C, "Cell")
         .def(py::init<bool, bool, bool, bool>())
-        .def("val" &Cell::val)
+        .def("val", &Cell::val)
         .def("compare", &Cell::compare)
         .def("setDirection", &Cell::setDirection)
         .def("updateDirection", &Cell::updateDirection);
@@ -22,6 +22,4 @@ PYBIND11_MODULE(Maze, M)
     
     py::class_<Maze>(M, "Maze")
         .def(py::init<int, int>());
-        
-    Maze.def()
 }
---
+++ test.py
@@ -0,0 +1,4 @@
+from Maze import Cell
+
+c = Cell.__init__(0,0,0,0)
+print(c)
