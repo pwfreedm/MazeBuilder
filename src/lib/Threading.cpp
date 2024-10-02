82b57e8544e8601699cdee4a9a19831f45a43564
Peter <frdmanp@gmail.com>
2024-10-02 16:44:42 -0400

    blocking is done

--- src/lib/Threading.cpp
+++ src/lib/Threading.cpp
@@ -6,15 +6,14 @@
 #include "../include/Wilsons.hpp"
 #include "../include/HK.hpp"
 
-
 Maze parallelize (std::string algo, int length, int width, int seed, int maxCores = std::thread::hardware_concurrency());
-std::vector<int>  block_dimensions (int length, int width, int numcores); 
+std::vector<int>  block_dimensions (int length, int numcores); 
 
 Maze
 parallelize (std::string algo, int length, int width, int seed, int numcores)
 {
     //if the requested maze is too small, do it serially
-    if (length < numcores * 2 || width < numcores * 2) 
+    if (length < numcores * 2 && width < numcores * 2) 
     {
        Maze mz = Maze(length, width); 
        if (algo == "Wilsons") { 
@@ -26,30 +25,39 @@ parallelize (std::string algo, int length, int width, int seed, int numcores)
       return mz; 
     }
 
-    std::vector<int> blocks = block_dimensions(length, width, numcores);
-    
+    std::vector<int> blocks = block_dimensions(length, numcores);
 
+    for (int i = 0; i < numcores; ++i)
+    {
+        //create a maze of size blocks[i], width
+        //pass it, algorithm, and seed to a new thread
+    }
+
+    //another for loop to wait for all the threads, recollecting as needed. 
+    //is ordering really necessary? 
+
+    //another loop to reconnect/smooth. 
+
+    return Maze(1,1);
 }
 
 std::vector<int>
-block_dimensions (int length, int width, int numcores)
+block_dimensions (int length, int numcores)
 {
     //figuring out the sizes of individual blocks
-    int blen, bwid, lenr, widr;
+    int blen, lenr;
     if ((lenr = length % numcores) == 0) { blen = length / numcores; }
     else { blen = (length - lenr) / numcores; }
 
-    if ((widr = width % numcores) == 0) { bwid = width / numcores; }
-    else { bwid = (width - widr) / numcores; }
     //returning length width pairs for each block
-    std::vector<int> blocks (numcores * 2);
-    for (int i = 0; i < numcores * 2; i += 2)
+    std::vector<int> blocks (numcores);
+    for (int i = 0; i < numcores; ++i)
     {
-        if (lenr && i >= (numcores * 2 - lenr)) { blocks[i] = blen + 1; }
+        if (lenr > 0) { 
+            blocks[i] = blen + 1; 
+            --lenr;
+        }
         else { blocks[i] = blen; }
-
-        if (widr && i >= (numcores * 2 - widr)) { blocks[i + 1] = bwid + 1; }
-        else { blocks[i + 1] = bwid; }
     }
     return blocks;
 }
