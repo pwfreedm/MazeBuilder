6570351907a8322bf2253096af7cf2947732d62e
Peter Freedman <frdmanp@gmail.com>
2024-11-29 10:45:49 -0500

    Profile script now works as intended - the "r" flag controls the number of times the length and width are stepped, the rn flag controls the number of mazes generated between steps.

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python mazebuilder.py -a hk -s 0 -w 64 -l 64 -c -o Parallel_HK -p -r 100 -ls 64 -ws 64 -rn 25
-python mazebuilder.py -a wilsons -s 0 -w 64 -l 64 -c -o Parallel_Wilsons -p -r 100 -ls 64 -ws 64 -rn 25
-python mazebuilder.py -a hk -s 0 -w 64 -l 64 -c -o Serial_HK -r 100 -ls 64 -ws 64 -rn 25
-python mazebuilder.py -a wilsons -s 0 -w 64 -l 64 -c -o Serial_Wilsons -r 100 -ls 64 -ws 64 -rn 25
+python mazebuilder.py -a hk -s 0 -w 64 -l 64 -c -o Parallel_HK -p -r 45 -ls 64 -ws 64 -rn 100
+python mazebuilder.py -a wilsons -s 0 -w 64 -l 64 -c -o Parallel_Wilsons -p -r 45 -ls 64 -ws 64 -rn 100
+python mazebuilder.py -a hk -s 0 -w 64 -l 64 -c -o Serial_HK -r 45 -ls 64 -ws 64 -rn 100
+python mazebuilder.py -a wilsons -s 0 -w 64 -l 64 -c -o Serial_Wilsons -r 45 -ls 64 -ws 64 -rn 100
